85e4dbf8cbd39df577ee82b755ccfd94065da4ce
Insar Miftahov <insarmiftahov@yandex.ru>
2021-04-23 19:24:37 +0300

    Добавлена 8 лабораторная и изменения в верстке

---
+++ lw8/JS/script1.js
@@ -0,0 +1,39 @@
+function WhichType(Digit) {
+  let Identifier;
+  for (let i = 2; i <= Digit; i++) {
+    Identifier = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) {
+        Identifier = false;
+        break;
+      }
+    }
+  }
+  if (Identifier) {
+    return console.log('Результат:', Digit, 'is prime number');
+  } else {
+      return console.log('Результат:', Digit, 'is not prime number');
+  }
+}
+
+function isPrimeNumber(Сontent) {
+  switch (typeof Сontent) {
+    case 'number':
+      return WhichType(Сontent);
+
+    case 'object':    
+      if (Сontent instanceof Array) {
+        for (let i = 0; i < Сontent.length; i++) {
+          WhichType(Сontent[i]); 
+        }
+        return console.log('Успех');
+      } else {
+          return console.log('Передан обьект, но не массив');
+      }
+
+    default:
+      return console.log('Некорректные входные данные');
+    }
+}
+
+console.log('Введите: isPrimeNumber(<значение>)');
